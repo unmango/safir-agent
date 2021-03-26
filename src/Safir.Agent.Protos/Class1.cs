434c4549d32a78ef2f98d6e2d9cc3a3749d25efa
Erik Rasmussen <ecr.referee@hotmail.com>
2021-03-26 00:11:37 -0500

    Setup proto stuff

--- src/Safir.Agent.Protos/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Safir.Agent.Protos
-{
-    public class Class1
-    {
-    }
-}
--- src/Safir.Agent/Services/GreeterService.cs
+++ src/Safir.Agent/Services/GreeterService.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 
-namespace Safir.Agent
+namespace Safir.Agent.Services
 {
     public class GreeterService : Greeter.GreeterBase
     {
         private readonly ILogger<GreeterService> _logger;
+        
         public GreeterService(ILogger<GreeterService> logger)
         {
             _logger = logger;
