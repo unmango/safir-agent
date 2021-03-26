b53df49970dadd17a2cd9e2b49ce01f4a65a8bd6
Erik Rasmussen <ecr.referee@hotmail.com>
2021-03-26 00:21:39 -0500

    Add server reflection

--- src/Safir.Agent/Startup.cs
+++ src/Safir.Agent/Startup.cs
@@ -18,6 +18,7 @@ namespace Safir.Agent
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+            services.AddGrpcReflection();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +34,11 @@ namespace Safir.Agent
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
+
+                if (env.IsDevelopment())
+                {
+                    endpoints.MapGrpcReflectionService();
+                }
 
                 endpoints.MapGet("/", async context =>
                 {
