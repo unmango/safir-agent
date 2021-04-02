12bf25ed0684362315ef355e0de213cf5cbfde07
Erik Rasmussen <ecr.referee@hotmail.com>
2021-04-01 20:07:04 -0500

    Add path wrapper

--- src/Safir.Agent/Startup.cs
+++ src/Safir.Agent/Startup.cs
@@ -37,6 +37,7 @@ namespace Safir.Agent
 
             services.AddTransient<IDirectory, SystemDirectoryWrapper>();
             services.AddTransient<IFile, SystemFileWrapper>();
+            services.AddTransient<IPath, SystemPathWrapper>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
